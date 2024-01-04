d0a12790e247784f74b31a9acc2a6c23f03cf39c
Portable-Polytech <mathieusemcot@gmail.com>
2024-01-04 12:35:02 +0100

    add max_list_int test and prototype

--- tests_funcs.py
+++ tests_funcs.py
@@ -2,12 +2,16 @@ import unittest
 import funcs
 
 class TestMaxListInt(unittest.TestCase):
-	def test_max_list_int(self):
+	def test_isequal(self):
+		self.assertEqual(funcs.max_list_int([1,1,1,1,1]), [1])
+
+	def test_classic(self):
 		self.assertEqual(funcs.max_list_int([0,1,2,3,4,5]), [3,4,5])
 		self.assertEqual(funcs.max_list_int([-5,-4,-3,-2,-1]), [-1,-2,-3])
-		self.assertEqual(funcs.max_list_int([-4,-3,-4,-3,-5]), [-5,-4,-3])
-		self.assertEqual(funcs.max_list_int([1,1,1,1,1]), [1])
 		self.assertEqual(funcs.max_list_int([-3, -7, 0, 2, -1]), [-1,0,2])
+
+	def test_double(self):
+		self.assertEqual(funcs.max_list_int([-4,-3,-4,-3,-5]), [-5,-4,-3])
 
 if __name__ == '__main__':
 	unittest.main()
