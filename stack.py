a528742e08685c66d8527ef071df5c399c6956ab
MathieuSemenzato <mathieusemcot@gmail.com>
2024-01-08 14:08:54 +0100

    add the corresponding lifo function

--- stack.py
+++ stack.py
@@ -29,7 +29,19 @@ class LIFO:
 		self.max_element = 3 # indique une taille maximale pour la pile
 
 	def insert_data_lifo(self, data):
-		pass
+		# on part du principe que data est un entier (une seule valeur à la fois) à inscrire
+		if len(self.values) == self.max_element:
+			self.values[len(self.values)-1] = data
+		else:
+			self.values.append(data)
+
+		return self.values
 
 	def remove_data_lifo(self):
-		pass
+		# on part du principe qu'une seule valeur est retirée à la fois
+		if len(self.values) == 0:
+			print("ne peut pas depiler car la pile est vide\n")
+		else:
+			del self.values[-1]
+
+		return self.values
