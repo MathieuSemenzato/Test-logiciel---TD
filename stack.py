2047327e9158a4d379760f3257f5b48011b6b106
MathieuSemenzato <mathieusemcot@gmail.com>
2024-01-08 14:00:41 +0100

    add the corresponding fifo function

--- stack.py
+++ stack.py
@@ -4,7 +4,21 @@ class Stack:
 		self.max_element = 3 # indique une taille maximale pour la pile
 	
 	def insert_data_fifo(self, data):
-		pass
+		# on part du principe que data est un entier (une seule valeur à la fois) à inscrire
+		if len(self.values) == self.max_element:
+			for i in range (len(self.values)-1):
+				self.values[i] = self.values[i+1]
+			self.values[len(self.values)-1] = data
+		else:
+			self.values.append(data)
+
+		return self.values
 
 	def remove_data_fifo(self):
-		pass
+		# on part du principe qu'une seule valeur est retirée à la fois
+		if len(self.values) == 0:
+			print("ne peut pas depiler car la pile est vide\n")
+		else:
+			del self.values[0]
+
+		return self.values
